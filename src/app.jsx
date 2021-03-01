bcf4efbe2bd32c7caa151a02c897b97c6d73c426
Jin Myung Park <52149400+myungtech@users.noreply.github.com>
2021-03-01 16:18:38 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,30 @@
+import React, { useEffect, useState } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return <h1>Hello :)</h1>;
+
+  //리액트훅에서 state를 사용할 수 있는 방법 useState
+  const [videos, setVideos] = useState([]); //비디오 목록
+
+  // 컴포넌트가 마운트, 업데이트 될 때마다 호출
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+
+    fetch(
+      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCRtqw_64AyzaJRP21VSLyFZkfcxRNdB-E",
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+
+  }, []);
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
