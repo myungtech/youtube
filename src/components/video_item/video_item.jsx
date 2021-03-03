663dca084d8e76ec3819dd3d217d5fafcec263b2
Jin Myung Park <52149400+myungtech@users.noreply.github.com>
2021-03-03 17:21:31 +0900

    make video item

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import styles from './video_item.module.css';
 
-const VideoItem = (props) => (
-    <h1>{props.video.snippet.title}</h1>
+// deconstruncting 가능하다. 
+//props 안에 video바로 받아오자 ( 이름이동일해야함 )
+const VideoItem = ({ video: { snippet } }) => (
+    <li className={styles.container}>
+        <div className={styles.video}>
+            <img
+                className={styles.thumbnail} src={snippet.thumbnails.medium.url}
+                alt="video thumbnail" />
+            <div className={styles.metadata}>
+                <p className={styles.title}>{snippet.title}</p>
+                <p className={styles.channel}>{snippet.channelTitle}</p>
+            </div >
+        </div>
+    </li >
 );
 
 export default VideoItem;
