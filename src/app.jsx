0a21381d47cdab937b04691dffaf27b6f75ea91b
Jin Myung Park <52149400+myungtech@users.noreply.github.com>
2021-03-07 18:14:52 +0900

    back to the list when searching

--- src/app.jsx
+++ src/app.jsx
@@ -2,26 +2,50 @@ import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({ youtube }) {
 
   //리액트훅에서 state를 사용할 수 있는 방법 useState
   const [videos, setVideos] = useState([]); //비디오 목록
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
+
   const search = (query) => {
+    setSelectedVideo(null);
     youtube.search(query)
       .then(videos => setVideos(videos));
+  };
 
-  }
   // 컴포넌트가 마운트, 업데이트 될 때마다 호출
   useEffect(() => {
     youtube.mostPopular()
       .then(videos => setVideos(videos));
   }, []);
 
+  //  selectedVideo가 있다면 div를 보여주고 없다면 안 보여지도록
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? 'list' : 'grid'}
+          />
+        </div>
+      </section>
     </div>);
 }
 
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -3,18 +3,26 @@ import styles from './video_item.module.css';
 
 // deconstruncting 가능하다. 
 //props 안에 video바로 받아오자 ( 이름이동일해야함 )
-const VideoItem = ({ video: { snippet } }) => (
-    <li className={styles.container}>
-        <div className={styles.video}>
-            <img
-                className={styles.thumbnail} src={snippet.thumbnails.medium.url}
-                alt="video thumbnail" />
-            <div className={styles.metadata}>
-                <p className={styles.title}>{snippet.title}</p>
-                <p className={styles.channel}>{snippet.channelTitle}</p>
-            </div >
-        </div>
-    </li >
-);
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+    const displayType = display === 'list' ? styles.list : styles.grid;
+    return (
+        <li
+            className={`${styles.container} ${displayType}`}
+            onClick={() => onVideoClick(video)}
+        >
+            <div className={styles.video}>
+                <img
+                    className={styles.thumbnail}
+                    src={snippet.thumbnails.medium.url}
+                    alt="video thumbnail"
+                />
+                <div className={styles.metadata}>
+                    <p className={styles.title}>{snippet.title}</p>
+                    <p className={styles.channel}>{snippet.channelTitle}</p>
+                </div>
+            </div>
+        </li>
+    )
+};
 
 export default VideoItem;
